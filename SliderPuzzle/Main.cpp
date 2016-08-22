821f6ce9ced5816fec07c9ca1eb7370f22104696
Reza Nourai <Reza Nourai>
2016-08-21 22:33:29 -0700

    Create and show a simple window.

--- SliderPuzzle/Main.cpp
+++ SliderPuzzle/Main.cpp
@@ -2,11 +2,87 @@
 #include <SpriteBatch.h>
 #include <memory>
 
+static HWND WindowInit(HINSTANCE instance, const wchar_t* class_name, int32_t width, int32_t height);
+static LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE, LPSTR, int)
 {
-  ID3D11DeviceContext* context = nullptr;
-  std::shared_ptr<DirectX::SpriteBatch> spriteBatch(new DirectX::SpriteBatch(context));
+  HWND window = WindowInit(instance, L"SliderPuzzle", 1280, 720);
+  if (!window)
+  {
+    return -1;
+  }
 
-  UNREFERENCED_PARAMETER(instance);
+  ShowWindow(window, SW_SHOW);
+  UpdateWindow(window);
+
+  //ID3D11DeviceContext* context = nullptr;
+  //std::shared_ptr<DirectX::SpriteBatch> spriteBatch(new DirectX::SpriteBatch(context));
+
+  MSG msg {};
+  while (msg.message != WM_QUIT)
+  {
+    if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+    {
+      TranslateMessage(&msg);
+      DispatchMessage(&msg);
+    }
+    else
+    {
+    }
+  }
+
+  DestroyWindow(window);
   return 0;
+}
+
+HWND WindowInit(HINSTANCE instance, const wchar_t* class_name, int32_t width, int32_t height)
+{
+  WNDCLASSEX wcx {};
+  wcx.cbSize = sizeof(wcx);
+  wcx.hbrBackground = (HBRUSH)GetStockObject(BLACK_BRUSH);
+  wcx.hInstance = instance;
+  wcx.lpfnWndProc = WindowProc;
+  wcx.lpszClassName = class_name;
+  if (RegisterClassEx(&wcx) == INVALID_ATOM)
+  {
+    assert(false);
+    return nullptr;
+  }
+
+  DWORD style = WS_OVERLAPPEDWINDOW;
+
+  RECT rc  {};
+  rc.right = width;
+  rc.bottom = height;
+  AdjustWindowRect(&rc, style, FALSE);
+
+  HWND hwnd = CreateWindow(wcx.lpszClassName, wcx.lpszClassName, style, CW_USEDEFAULT, CW_USEDEFAULT,
+    rc.right - rc.left, rc.bottom - rc.top, nullptr, nullptr, instance, nullptr);
+  if (!hwnd)
+  {
+    assert(false);
+    return nullptr;
+  }
+
+  return hwnd;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+  switch (msg)
+  {
+  case WM_CLOSE:
+    PostQuitMessage(0);
+    break;
+
+  case WM_KEYDOWN:
+    if (wParam == VK_ESCAPE)
+    {
+      PostQuitMessage(0);
+    }
+    break;
+  }
+
+  return DefWindowProc(hwnd, msg, wParam, lParam);
 }
